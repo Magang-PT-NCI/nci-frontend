b7ac93cfad96c3c72e1a700e76905b6dac2d1ac1
ZaAtsmaro28 <fazaatsmaro@gmail.com>
2024-10-31 13:31:54 +0700

    create confirm presence feeature and overtime feature

---
+++ src/components/ConfirmationForm.tsx
@@ -0,0 +1,201 @@
+import {
+  View,
+  Text,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Alert,
+} from 'react-native';
+import React, { useState } from 'react';
+import ButtonCustom from './ButtonCustom';
+import DropdownCustom from './DropdownCustom';
+import { Ionicons } from '@expo/vector-icons';
+import { getCookie } from '../utils/getCookie';
+import ApiRequest from '../utils/ApiRequest';
+import { OvertimeResData } from '../interfaces/overtime.dto';
+import { Endpoint } from '../enums/endpoint-class';
+import { StackActions, useNavigation } from '@react-navigation/native';
+import FileInput from './FileInput';
+import * as DocumentPicker from 'expo-document-picker';
+import { AttendanceResData } from '../interfaces/attendance.dto';
+
+interface ConfirmationFormProps {
+  setIsModalOpen: (state: boolean) => void;
+  isModalOpen: boolean;
+  attendanceData: AttendanceResData;
+}
+
+const ConfirmationForm: React.FC<ConfirmationFormProps> = ({
+  setIsModalOpen,
+  isModalOpen,
+  attendanceData,
+}) => {
+  const [selectedType, setSelectedType] = useState<string>('');
+  const [selectedReason, setSelectedReason] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [selectedFile, setSelectedFile] =
+    useState<DocumentPicker.DocumentPickerResult | null>(null);
+
+  const navigation = useNavigation();
+
+  const confirm_type = [
+    { label: 'Check in', value: 'check_in' },
+    { label: 'Check out', value: 'check_out' },
+    { label: 'Izin', value: 'permit' },
+  ];
+
+  const confirm_reason = [
+    { label: 'Sakit', value: 'sakit' },
+    { label: 'Urusan mendadak', value: 'urusan_mendadak' },
+    { label: 'Cuti', value: 'cuti' },
+    { label: 'Duka', value: 'duka' },
+    { label: 'Melahirkan', value: 'melahirkan' },
+    { label: 'Lainnya', value: 'lainnya' },
+  ];
+
+  const handleTypeChange = (value: string) => {
+    setSelectedType(value);
+  };
+
+  const handleReasonChange = (value: string) => {
+    setSelectedReason(value);
+  };
+
+  const isFormValid = () => {
+    return (
+      confirm_type !== null &&
+      confirm_reason !== null &&
+      description.trim() !== '' &&
+      selectedFile !== null
+    );
+  };
+
+  const handleSubmitConfirmation = async () => {
+    const token = await getCookie('token');
+    const formdata = new FormData();
+
+    const file = selectedFile?.assets[0];
+
+    if (token) {
+      formdata.append('attendance_id', `${attendanceData.id}`);
+      formdata.append('type', selectedType);
+      formdata.append('reason', selectedReason);
+      formdata.append('description', description);
+      formdata.append('attachment', {
+        uri: file.uri,
+        name: file.name,
+        type: file.mimeType,
+      } as unknown as Blob);
+
+      await new ApiRequest<FormData, OvertimeResData>(Endpoint.ValidatePresence)
+        .setToken(token)
+        .setContentType('multipart/form-data')
+        .setReqBody(formdata)
+        .post(
+          (data) => {
+            Alert.alert(
+              'Pemberitahuan',
+              'Konfirmasi kehadiran berhasil dikirim',
+              [
+                {
+                  text: 'Mengerti',
+                  onPress: () => {
+                    console.log(data);
+                  },
+                },
+              ],
+            );
+          },
+          (error) => {
+            Alert.alert('Pemberitahuan', error.message, [
+              {
+                text: 'Mengerti',
+                onPress: () => {},
+              },
+            ]);
+          },
+        );
+    } else {
+      navigation.dispatch(StackActions.replace('LoginPage'));
+    }
+  };
+
+  const handleFileSelect = (file: DocumentPicker.DocumentPickerResult) => {
+    setSelectedFile(file);
+  };
+
+  return (
+    <View>
+      <Modal
+        visible={isModalOpen}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setIsModalOpen(false)}
+      >
+        <TouchableWithoutFeedback onPress={() => setIsModalOpen(false)}>
+          <View className="h-full p-4 bg-black/60">
+            <TouchableWithoutFeedback>
+              <View
+                className="w-full border rounded-lg p-5 py-10 my-auto"
+                style={{ borderColor: '#5cb874', backgroundColor: '#0f172a' }}
+              >
+                <View className="w-full flex-row justify-between items-center">
+                  <Text className="text-accentYellow text-lg">
+                    Formulir Konfirmasi Absensi
+                  </Text>
+
+                  {/*close-modal-button*/}
+                  <TouchableOpacity onPress={() => setIsModalOpen(false)}>
+                    <Ionicons name="close" size={37} color="#94a3b8" />
+                  </TouchableOpacity>
+                </View>
+
+                {/*dropdown-type*/}
+                <DropdownCustom
+                  data={confirm_type}
+                  onValueChange={handleTypeChange}
+                  placeholder="Pilih jenis konfirmasi"
+                  marginT={16}
+                />
+
+                {/*dropdown-reason*/}
+                <DropdownCustom
+                  isDisabled={selectedType !== 'permit'}
+                  data={confirm_reason}
+                  onValueChange={handleReasonChange}
+                  placeholder="Pilih alasan"
+                  marginT={16}
+                />
+
+                {/*description*/}
+                <TextInput
+                  multiline={true}
+                  numberOfLines={3}
+                  className="border border-accentYellow rounded-lg mt-7 px-3 text-textDefault"
+                  onChangeText={(text) => setDescription(text)}
+                  value={description}
+                  placeholder={'Deskripsi konfirmasi kehadiran'}
+                  placeholderTextColor={'#94a3b8'}
+                />
+
+                {/*attachment*/}
+                <FileInput onFileSelect={handleFileSelect} />
+
+                {/*button-submit*/}
+                <ButtonCustom
+                  title="Kirim"
+                  callbackEvt={handleSubmitConfirmation}
+                  mt={20}
+                  disabled={!isFormValid()}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    </View>
+  );
+};
+
+export default ConfirmationForm;
--- src/pages/OnSitePage/AttendancePage.tsx
+++ src/pages/OnSitePage/AttendancePage.tsx
@@ -1,56 +1,62 @@
-import { useEffect, useState } from "react";
-import { View, Text, ScrollView, Alert, Linking } from "react-native";
-import React from "react";
-import ButtonCustom from "../../components/ButtonCustom";
-import EmployeeInfo from "../../components/EmployeeInfo";
-import AttendanceHistory from "../../components/AttendanceHistory";
-import CameraComponent from "../../components/CameraComponent/CameraComponent";
-import ImageAttendance from "../../components/ImageAttendance";
-import ApiRequest from "../../utils/ApiRequest";
-import { getCookie } from "../../utils/getCookie";
-import { HeadersReq } from "../../interfaces/api-request";
-import {
-  EmployeeResData,
-} from "../../interfaces/employee.dto";
-import { Endpoint } from "../../enums/api-enum";
+import { useEffect, useState } from 'react';
+import { View, Text, ScrollView, Alert, TouchableOpacity } from 'react-native';
+import React from 'react';
+import ButtonCustom from '../../components/ButtonCustom';
+import EmployeeInfo from '../../components/EmployeeInfo';
+import AttendanceHistory from '../../components/AttendanceHistory';
+import CameraComponent from '../../components/CameraComponent/CameraComponent';
+import ImageAttendance from '../../components/ImageAttendance';
+import ApiRequest from '../../utils/ApiRequest';
+import { getCookie } from '../../utils/getCookie';
+import { HeadersReq } from '../../interfaces/api-request';
+import { EmployeeResData } from '../../interfaces/employee.dto';
+import { Endpoint } from '../../enums/endpoint-class';
 import {
   getLocation,
   isEmpty,
   requestCameraPermission,
-  workingHours,
-} from "../../utils/commonUtils";
-import dayjs from "dayjs";
-import { useNavigation } from "@react-navigation/native";
-import {AttendanceCheckRes, AttendancePostRes, AttendanceResData} from "../../interfaces/attendance.dto";
-import AttendanceMap from "../../components/AttendanceMap";
+} from '../../utils/commonUtils';
+import { StackActions, useNavigation } from '@react-navigation/native';
+import {
+  AttendanceCheckRes,
+  AttendancePostRes,
+  AttendanceResData,
+} from '../../interfaces/attendance.dto';
+import AttendanceMap from '../../components/AttendanceMap';
+import { Ionicons } from '@expo/vector-icons';
+import { OvertimeReq, OvertimeResData } from '../../interfaces/overtime.dto';
+import ConfirmationForm from '../../components/ConfirmationForm';
 
 type AttendancePageProps = {
   NIK: string;
   status: number;
   attendanceData: AttendanceResData;
+  isCheckedIn: boolean;
+  setIsCheckedIn: (isCheckedIn: boolean) => void;
+  setAttendanceID: (id: number) => void;
 };
 
 const AttendancePage: React.FC<AttendancePageProps> = ({
   NIK,
   status,
   attendanceData,
+  isCheckedIn,
+  setIsCheckedIn,
+  setAttendanceID,
 }) => {
   const [cameraVisible, setCameraVisible] = useState(false);
-  const [isCheckedIn, setIsCheckedIn] = useState(false);
-  const [capturedPhoto, setCapturedPhoto] = useState<string | null>(null);
   const [employee, setEmployee] = useState({} as EmployeeResData);
-  const [isButtonCheckDisable, setIsButtonCheckDisable] = useState(true);
   const [checkResData, setCheckResData] = useState<{
-    checkIn: AttendanceCheckRes | null, checkOut: AttendanceCheckRes | null}>({checkIn: null, checkOut: null});
-
-  const currentTime = dayjs();
-  const startWorkTime = dayjs().hour(6).minute(0).second(0).millisecond(0);
+    checkIn: AttendanceCheckRes | null;
+    checkOut: AttendanceCheckRes | null;
+  }>({ checkIn: null, checkOut: null });
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const getEmployeeData = async () => {
-    const token = await getCookie("token");
+    const token = await getCookie('token');
     if (token) {
       const res = await new ApiRequest<HeadersReq, EmployeeResData>(
-        Endpoint.GetEmployee
+        Endpoint.GetEmployee,
       )
         .setToken(token)
         .setPathParam(NIK)
@@ -72,85 +78,76 @@ const AttendancePage: React.FC<AttendancePageProps> = ({
     } else {
       setCameraVisible(true);
     }
-
   };
 
   const navigation = useNavigation();
   const handlePhotoCapture = async (photo: string) => {
+    setCameraVisible(false);
 
-  setCapturedPhoto(photo);
-  setCameraVisible(false);
-  setIsCheckedIn(true);
+    const token = await getCookie('token');
+    const location = await getLocation();
+    const formData = new FormData();
+    const type = checkResData.checkIn ? 'check_out' : 'check_in';
 
-  const token = await getCookie("token");
-  const location = await getLocation();
-  const formData = new FormData();
+    if (token && location) {
+      formData.append('nik', NIK);
+      formData.append('type', type);
+      formData.append('location', JSON.stringify(location));
+      formData.append('photo', {
+        uri: photo,
+        name: 'photo.jpg',
+        type: 'image/jpeg',
+      } as unknown as Blob);
 
-  if (token && location) {
-    formData.append("nik", NIK);
-    formData.append(
-      "type",
-      isEmpty(attendanceData) ? "check_in" : "check_out"
-    );
-    formData.append("location", JSON.stringify(location));
-    formData.append("photo", {
-      uri: photo,
-      name: "photo.jpg",
-      type: "image/jpeg",
-    });
-
-      const response = await new ApiRequest<FormData, AttendancePostRes>(Endpoint.Attendance)
+      const response = await new ApiRequest<FormData, AttendancePostRes>(
+        Endpoint.Attendance,
+      )
         .setToken(token)
-        .setContentType("multipart/form-data")
+        .setContentType('multipart/form-data')
         .setReqBody(formData)
         .post(
-          (data) => console.log(data),
+          () => {
+            setIsCheckedIn(type === 'check_in');
+          },
           (error) => {
-            console.log(JSON.stringify(error, null, 2))
+            console.log(JSON.stringify(error, null, 2));
 
-            Alert.alert(
-                "Peringatan",
-                error.message,
-                [
-                  {
-                    text: "Mengerti",
-                    onPress: () => {
-
-                    },
-                  },
-                ]
-            );
-          }
+            Alert.alert('Peringatan', error.message, [
+              {
+                text: 'Mengerti',
+                onPress: () => {},
+              },
+            ]);
+          },
         );
 
       const postResData = response.getData();
+      setAttendanceID(postResData.attendance_id);
+      console.log(postResData.attendance_id);
 
-
-        setCheckResData((prevData)=> {
-          if(postResData.type === "check_in") {
-            return {
-              checkOut: prevData.checkOut,
-              checkIn: {
-                time: postResData.time,
-                photo: postResData.photo,
-                location: postResData.location,
-              }
-            }
-          }
-          else {
-            return {
-              checkIn:prevData.checkIn,
-              checkOut: {
-                time: postResData.time,
-                photo: postResData.photo,
-                location: postResData.location,
-              }
-            }
-          }
-        })
-
+      setCheckResData((prevData) => {
+        if (postResData.type === 'check_in') {
+          return {
+            checkOut: prevData.checkOut,
+            checkIn: {
+              time: postResData.time,
+              photo: postResData.photo,
+              location: postResData.location,
+            },
+          };
+        } else {
+          return {
+            checkIn: prevData.checkIn,
+            checkOut: {
+              time: postResData.time,
+              photo: postResData.photo,
+              location: postResData.location,
+            },
+          };
+        }
+      });
     } else {
-      navigation.replace("LoginPage");
+      navigation.dispatch(StackActions.replace('LoginPage'));
     }
   };
 
@@ -158,7 +155,40 @@ const AttendancePage: React.FC<AttendancePageProps> = ({
     setCameraVisible(false);
   };
 
+  const handleOvertime = async () => {
+    const token = await getCookie('token');
 
+    if (token) {
+      await new ApiRequest<OvertimeReq, OvertimeResData>(Endpoint.Overtime)
+        .setToken(token)
+        .setReqBody({ nik: NIK })
+        .post(
+          () => {
+            Alert.alert('Pemberitahuan', 'Pengajuan LEMBUR berhasil dikirim', [
+              {
+                text: 'Mengerti',
+                onPress: () => {},
+              },
+            ]);
+          },
+          (error) => {
+            Alert.alert('Pemberitahuan', error.message, [
+              {
+                text: 'Mengerti',
+                onPress: () => {},
+              },
+            ]);
+          },
+        );
+    } else {
+      navigation.dispatch(StackActions.replace('LoginPage'));
+    }
+  };
+
+  const handlePresenceConfirmation = async () => {
+    setIsModalOpen(true);
+    console.log('[AttendancePage] TEST - Open modal confirm presence');
+  };
 
   useEffect(() => {
     getLocation();
@@ -166,36 +196,31 @@ const AttendancePage: React.FC<AttendancePageProps> = ({
     if (NIK) {
       getEmployeeData();
     }
-
-    if (workingHours(currentTime, startWorkTime)) {
-      setIsButtonCheckDisable(false);
-    }
   }, [NIK]);
 
   useEffect(() => {
     if (!isEmpty(attendanceData)) {
       setIsCheckedIn(!attendanceData.checkIn);
-      setIsButtonCheckDisable(!!attendanceData.checkOut);
     }
 
-    setCheckResData({checkIn:attendanceData.checkIn, checkOut: attendanceData.checkOut});
+    setCheckResData({
+      checkIn: attendanceData.checkIn,
+      checkOut: attendanceData.checkOut,
+    });
 
-    setIsCheckedIn(!(isEmpty(attendanceData) || !(attendanceData?.checkIn)));
-
+    setIsCheckedIn(!(isEmpty(attendanceData) || !attendanceData?.checkIn));
   }, [attendanceData]);
 
   if (isEmpty(attendanceData) && status !== 204) {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ color: "#fff" }}>Loading attendance data...</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: '#fff' }}>Loading attendance data...</Text>
       </View>
     );
   }
 
-  console.log(JSON.stringify(attendanceData,null, 2));
-  console.log()
-
   return (
+    // @ts-ignore
     <ScrollView style={{ showsVerticalScrollIndicator: false }}>
       <View className="w-full h-full flex bg-background px-4">
         <Text className="font-bold text-xl text-accentYellow text-left my-4">
@@ -209,10 +234,41 @@ const AttendancePage: React.FC<AttendancePageProps> = ({
         />
 
         <ButtonCustom
-          title={isCheckedIn ? "Check Out" : "Check In"}
+          icon={'hand-left-outline'}
+          title={isCheckedIn ? 'Check Out' : 'Check In'}
+          isDisabled={!!attendanceData.checkOut}
           callbackEvt={handlerAttendance}
           mt={20}
+        />
 
+        <View className="px-2 flex flex-row mt-4 justify-center">
+          <TouchableOpacity
+            onPress={handleOvertime}
+            className="w-1/2 bg-accentGreen rounded-lg p-2 items-center mr-3"
+          >
+            <Ionicons name="newspaper-outline" size={24} color="black" />
+            <Text className="text-background text-xs text-center">
+              Pengajuan Lembur
+            </Text>
+          </TouchableOpacity>
+
+          {/*confirm-absent-button*/}
+          <TouchableOpacity
+            onPress={handlePresenceConfirmation}
+            disabled={!checkResData.checkIn}
+            className={`w-1/2 ${checkResData.checkIn ? 'bg-accentGreen' : 'bg-[#387D4A]'} rounded-lg p-2 items-center`}
+          >
+            <Ionicons name="checkmark-circle-outline" size={24} color="black" />
+            <Text className="text-background text-xs text-center">
+              Konfirmasi Kehadiran
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <ConfirmationForm
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          attendanceData={attendanceData}
         />
 
         <AttendanceHistory
@@ -233,20 +289,19 @@ const AttendancePage: React.FC<AttendancePageProps> = ({
         />
 
         {/*check in location*/}
-        {/*<AttendanceMap*/}
-        {/*    status={"Check In"}*/}
-        {/*    latitude={checkResData.checkIn?.location?.latitude}*/}
-        {/*    longitude={checkResData.checkIn?.location?.longitude}/>*/}
+        <AttendanceMap
+          status={'Check In'}
+          latitude={checkResData.checkIn?.location?.latitude}
+          longitude={checkResData.checkIn?.location?.longitude}
+        />
 
         {/*/!*check out location*!/*/}
-        {/*<AttendanceMap*/}
-        {/*    status={"Check Out"}*/}
-        {/*    latitude={checkResData.checkOut?.location?.latitude}*/}
-        {/*    longitude={checkResData.checkOut?.location?.longitude}/>*/}
+        <AttendanceMap
+          status={'Check Out'}
+          latitude={checkResData.checkOut?.location?.latitude}
+          longitude={checkResData.checkOut?.location?.longitude}
+        />
       </View>
-
-
-
     </ScrollView>
   );
 };
