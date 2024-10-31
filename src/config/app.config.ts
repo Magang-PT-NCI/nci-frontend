43b104efa5261e6eb6c25b1d8ef65ef4cb7fc696
ZaAtsmaro28 <fazaatsmaro@gmail.com>
2024-10-31 13:22:43 +0700

    backend using ngrok api

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,3 +1,5 @@
-export const EMPLOYEE_URL = process.env.EXPO_PUBLIC_EMPLOYEE_URL || 'http://localhost:3001'
-export const ATTENDANCE_URL = process.env.EXPO_PUBLIC_ATTENDANCE_URL || 'http://localhost:3002'
-export const API_KEY = process.env.EXPO_PUBLIC_API_KEY
+export const EMPLOYEE_URL =
+  process.env.EXPO_PUBLIC_EMPLOYEE_URL || 'http://localhost:3001';
+export const ATTENDANCE_URL =
+  process.env.EXPO_PUBLIC_ATTENDANCE_URL || 'http://localhost:3002';
+export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
